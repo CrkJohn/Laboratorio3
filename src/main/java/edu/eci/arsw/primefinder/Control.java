6c21e17b97e3df9b6fb2ff6542123b992a6ff8a3
CrkJohn <JhonhRge@gmail.com>
2019-02-05 22:33:21 -0500

    gitIgnore
    Co-authored-by: CamiloVelandia <jcamilovelandiab@gmail.com>

---
+++ src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.primefinder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ */
+public class Control extends Thread {
+
+	private final static int NTHREADS = 3;
+	private final static int MAXVALUE = 30000000;
+	private final static int TMILISECONDS = 5000;
+	private static int primesFound = 0;
+	private final int NDATA = MAXVALUE / NTHREADS;
+	private static Object sync = new Object();
+
+	private PrimeFinderThread pft[];
+
+	private Control() {
+		super();
+		this.pft = new PrimeFinderThread[NTHREADS];
+
+		int i;
+		for (i = 0; i < NTHREADS - 1; i++) {
+			PrimeFinderThread elem = new PrimeFinderThread(i * NDATA, (i + 1) * NDATA);
+			pft[i] = elem;
+		}
+		pft[i] = new PrimeFinderThread(i * NDATA, MAXVALUE + 1);
+	}
+
+	public static Control newControl() {
+		return new Control();
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < NTHREADS; i++) {
+			pft[i].start();
+		}
+		while (true){
+			for (PrimeFinderThread primeFinderThread : pft) {
+				primeFinderThread.setWait(true);
+				primesFound += (primeFinderThread.getPrimes().size());
+			}
+			synchronized (sync) {
+				sync.notifyAll();
+			}
+			System.out.println("Total de los primos son : " + primesFound);
+			try {
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				System.out.println("Enter");
+				br.readLine();
+				Thread.sleep(TMILISECONDS);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -0,0 +1,64 @@
+package edu.eci.arsw.primefinder;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class PrimeFinderThread extends Thread {
+
+	int a, b;
+	private static Object sync = new Object();
+
+	private List<Integer> primes;
+	private boolean isWait;
+
+	public PrimeFinderThread(int a, int b) {
+		super();
+		this.primes = new LinkedList<>();
+		this.a = a;
+		this.b = b;
+		isWait = true;
+	}
+
+	@Override
+	public void run() {
+		for (int i = a; i < b; i++) {
+			if (isPrime(i)) {
+				primes.add(i);
+//				System.out.println(i);
+			}
+		}
+		synchronized (sync) {
+			if (isWait) {
+				try {
+					sync.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			isWait = false;
+		}
+	}
+
+	boolean isPrime(int n) {
+		boolean ans;
+		if (n > 2) {
+			ans = n % 2 != 0;
+			for (int i = 3; ans && i * i <= n; i += 2) {
+				ans = n % i != 0;
+			}
+		} else {
+			ans = n == 2;
+		}
+		return ans;
+	}
+
+	public List<Integer> getPrimes() {
+		return primes;
+	}
+	
+	public void setWait(boolean newWait) {
+		 isWait = true;
+	}
+
+}
