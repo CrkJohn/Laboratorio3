9054da10de32c1853feb09ff1558b50471a65c6b
jcamilovelandiab <jcamilovelandiab@gmail.com>
2019-02-06 11:31:30 -0500

    botonesSnake
    Co-authored-by: John <JhonhRge@gmail.com>

---
+++ src/main/java/edu/eci/arsw/enums/GridSize.java
@@ -0,0 +1,9 @@
+package edu.eci.arsw.enums;
+
+public class GridSize {
+
+	public static final int HEIGH_BOX = 20;
+	public static final int WIDTH_BOX = 20;
+	public static final int GRID_WIDTH = 30;
+	public static final int GRID_HEIGHT = 30;
+}
